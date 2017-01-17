0965a393d2f94db9353db8016194f3a00661324c
Brett Vitaz <vitazbt@uw.edu>
2017-01-17 15:47:30 -0800

    add to and enhance schemas, clean up and refactor routes, add route for adding user and user pokemon

--- api/app.py
+++ api/app.py
@@ -2,7 +2,7 @@ from api.database import setup_database
 from api.database.models import pokedex
 # from api.data_import.data_import import import_all_data
 
-from flask import Flask, request, session, abort
+from flask import Flask, request, session, abort, jsonify
 from flask_marshmallow import Marshmallow
 from sqlalchemy.orm import exc
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -26,7 +26,17 @@ class CategorySchema(ma.ModelSchema):
         model = pokedex.Category
 
 
+class TypeSchema(ma.ModelSchema):
+    class Meta:
+        model = pokedex.Type
+
+
 class PokemonSchema(ma.ModelSchema):
+    fast_attacks = ma.Nested('AttackSchema', many=True, exclude=('pokemon',))
+    charge_attacks = ma.Nested('AttackSchema', many=True, exclude=('pokemon',))
+    category = ma.Nested('CategorySchema')
+    type = ma.Nested('TypeSchema', many=True, exclude=('pokemon',))
+
     class Meta:
         model = pokedex.Pokemon
 
@@ -37,64 +47,119 @@ class AttackSchema(ma.ModelSchema):
 
 
 class UserSchema(ma.ModelSchema):
+    email = ma.Email()
+    password = ma.String(load_only=True)
+    pokemon = ma.Nested('UserPokemonSchema', many=True, exclude=('user',))
+
     class Meta:
         model = pokedex.User
 
 
-@app.route('/api/login', methods=['GET', 'POST'])
+class UserPokemonSchema(ma.ModelSchema):
+    user = ma.Nested('UserSchema')
+    pokemon = ma.Nested('PokemonSchema')
+
+    class Meta:
+        model = pokedex.UserPokemon
+
+
+# TODO - implement non session based authentication
+@app.route('/api/login', methods=['POST'])
 def route_login():
-    error=None
     if request.method == 'POST':
         try:
-            user = pokedex.User.query.filter_by(name=request.form['username']).one()
+            user = pokedex.User.query.filter_by(username=request.form['username']).one()
         except exc.NoResultFound:
-            error = 'Invalid username'
+            pass  # user not found
         else:
             if check_password_hash(user.password, request.form['password']):
-                session['logged_in'] = user.name
-                return 'Logged in'
-            return 'Invalid password'
-    return error, 500
+                session['logged_in'] = True
+                session['user_id'] = user.id
+                session['username'] = user.username
+                return jsonify({'message': 'Logged in'})
+    return jsonify({'message': 'Invalid credentials'}), 422
 
 
 @app.route('/api/logout')
 def route_logout():
     if session.get('logged_in'):
-        session.pop('logged_in', None)
-        return 'Success'
+        session.clear()
+        return jsonify({'message': 'Logged out'})
     else:
-        return 'Already logged out.'
+        return jsonify({'message': 'Already logged out'})
 
 
-@app.route('/api/user', methods=['POST'])
+@app.route('/api/users', methods=['POST'])
 def route_user():
-    password_hash = generate_password_hash(request.form['password'])
-    user = pokedex.User(name=request.form['username'], password=password_hash)
+    schema = UserSchema(only=('username', 'password', 'email'))
+    user, errors = schema.load(request.form)
+    if errors:
+        return jsonify(errors=errors), 422
+    user.password = generate_password_hash(user.password)
     try:
         db.session.add(user)
         db.session.commit()
+        return jsonify({'message': 'Created new user.', 'user': schema.dump(user).data})
     except:
         db.session.rollback()
-        abort(500)
-    finally:
-        db.session.close()
-    return 'Success'
+        raise
 
 
-@app.route('/api/user/<username>')
-def route_get_user(username):
+# TODO - require authentication for most routes below
+@app.route('/api/users/<user_id>', methods=['GET', 'PUT'])
+def route_get_user(user_id):
+    schema = UserSchema()
     try:
-        user = pokedex.User.query.filter_by(name=username).one()
+        user = pokedex.User.query.filter_by(id=user_id).one()
     except (exc.NoResultFound, exc.MultipleResultsFound):
         abort(404)
-    return UserSchema().jsonify(user)
+
+    if request.method == 'GET':
+        return jsonify({'user': schema.dump(user).data})
+
+    if request.method == 'PUT':
+        user_update, errors = schema.load(request.form, instance=user, partial=True)
+        if errors:
+            return jsonify(errors=errors), 422
+        try:
+            db.session.add(user_update)
+            db.session.commit()
+            return jsonify({'message': 'Updated user.', 'user': schema.dump(user_update).data})
+        except:
+            db.session.rollback()
+            raise
 
 
-@app.route('/api/pokemon')
-def route_all_pokemon():
-    pokemon_schema = PokemonSchema(many=True)
-    all_pokemon = pokedex.Pokemon.query.all()
-    return pokemon_schema.jsonify(all_pokemon)
+@app.route('/api/users/<int:user_id>/pokemon', methods=['GET', 'POST'])
+def route_user_pokemon(user_id):
+    try:
+        user = pokedex.User.query.filter_by(id=user_id).one()
+    except (exc.NoResultFound, exc.MultipleResultsFound):
+        abort(404)
+
+    if request.method == 'GET':
+        schema = UserPokemonSchema(many=True)
+        return jsonify({'userPokemon': schema.dump(user.pokemon).data})
+
+    if request.method == 'POST':
+        schema = UserPokemonSchema()
+        user_pokemon, errors = schema.load(request.form)
+        user_pokemon.user_id = user_id
+        user_pokemon.pokemon_id = request.form['pokemon_id']
+        if errors:
+            return jsonify(errors=errors), 422
+        try:
+            db.session.add(user_pokemon)
+            db.session.commit()
+            return jsonify({'userPokemon': schema.dump(user_pokemon).data})
+        except:
+            db.session.rollback()
+            raise
+
+
+@app.route('/api/users/<int:user_id>/pokemon/<int:pokemon_id>', methods=['GET', 'PUT'])
+def route_get_user_pokemon(user_id, pokemon_id):
+    pass
 
 
 @app.route('/api/pokemon/<string:name>')
